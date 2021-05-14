c66e3335b3440180d705e2ed009c460146eb7192
Takashi Binns <takashi.binns@icloud.com>
2021-05-14 17:08:34 -0400

    adding simple_salesforce as dependency

--- setup.py
+++ setup.py
@@ -90,6 +90,7 @@ def setup_package():
             "tornado",
             "twisted",
             "urllib3",
+            "simple_salesforce",
         ],
         entry_points={
             "console_scripts": [
